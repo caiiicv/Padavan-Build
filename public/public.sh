534bbae5bfa6c03941f6061c3b42fb3785e711ae
caiiicv <62830679+caiiicv@users.noreply.github.com>
2021-08-16 08:32:37 +0800

    Update public.sh

--- public/public.sh
+++ public/public.sh
@@ -20,16 +20,16 @@ echo "修改用户名"
 sed -i 's/SYS_USER_ROOT		"admin"/SYS_USER_ROOT		"'$user_name'"/g' $default_file
 
 echo "修改登陆密码"
-sed -i 's/DEF_ROOT_PASSWORD	"admin"/DEF_ROOT_PASSWORD	"'$user_password'"/g' $default_file
+sed -i 's/DEF_ROOT_PASSWORD	"deng123"/DEF_ROOT_PASSWORD	"'$user_password'"/g' $default_file
 
 echo "修改 LAN IP 地址"
-sed -i "s/192.168.2/$lan_ip/g" $default_file
+sed -i "s/192.168.10/$lan_ip/g" $default_file
 
 echo "修改 2G 无线名称"
-sed -i 's/DEF_WLAN_2G_SSID	BOARD_PID "_%s"/DEF_WLAN_2G_SSID	"'$wlan_2g_ssid'"/g' $default_file
+sed -i 's/DEF_WLAN_2G_SSID	BOARD_PID "dem"/DEF_WLAN_2G_SSID	"'$wlan_2g_ssid'"/g' $default_file
 
 echo "修改 5G 无线名称"
-sed -i 's/DEF_WLAN_5G_SSID	BOARD_PID "_5G_%s"/DEF_WLAN_5G_SSID	"'$wlan_5g_ssid'"/g' $default_file
+sed -i 's/DEF_WLAN_5G_SSID	BOARD_PID "dem"/DEF_WLAN_5G_SSID	"'$wlan_5g_ssid'"/g' $default_file
 
 echo "修改 2G 访客无线名称"
 sed -i 's/DEF_WLAN_2G_GSSID	BOARD_PID "_GUEST_%s"/DEF_WLAN_2G_GSSID	"'$wlan_guest_2g_ssid'"/g' $default_file
@@ -38,10 +38,10 @@ echo "修改 5G 访客无线名称"
 sed -i 's/DEF_WLAN_5G_GSSID	BOARD_PID "_GUEST_5G_%s"/DEF_WLAN_5G_GSSID	"'$wlan_guest_5g_ssid'"/g' $default_file
 
 echo "修改 2.4GHz WIFI 密码"
-sed -i 's/DEF_WLAN_2G_PSK		"1234567890"/DEF_WLAN_2G_PSK		"'$wlan_2g_psk'"/g' $default_file
+sed -i 's/DEF_WLAN_2G_PSK		"dem666666"/DEF_WLAN_2G_PSK		"'$wlan_2g_psk'"/g' $default_file
 
 echo "修改 5GHz WIFI 密码"
-sed -i 's/DEF_WLAN_5G_PSK		"1234567890"/DEF_WLAN_5G_PSK		"'$wlan_5g_psk'"/g' $default_file
+sed -i 's/DEF_WLAN_5G_PSK		"dem666666"/DEF_WLAN_5G_PSK		"'$wlan_5g_psk'"/g' $default_file
 
 echo "更新版本号时间"
 sed -i "s/FIRMWARE_BUILDS_REV=.*/FIRMWARE_BUILDS_REV=$version_time/g" ./versions.inc
